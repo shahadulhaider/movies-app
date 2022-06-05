069177c8da7281c46d6fb4b09d540ee0dfc27fc7
Shahadul Haider <shahadul.haider@gmail.com>
2022-06-05 10:54:55 +0600

    logging interceptor

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,14 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './shared/logging-interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new LoggingInterceptor());
 
   const config = app.get(ConfigService);
   const port = config.get('port');
