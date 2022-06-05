13ea63d25dd175c825c8a2effd48ceb0e0d8ca22
Shahadul Haider <shahadul.haider@gmail.com>
2022-06-05 17:06:12 +0600

    update user decorator

--- backend/src/auth/user-decorator.ts
+++ backend/src/auth/user-decorator.ts
@@ -1,7 +1,11 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User as UserEntity } from 'src/users/user.entity';
 
-export const User = createParamDecorator((_: any, ctx: ExecutionContext) => {
-  const req = ctx.switchToHttp().getRequest();
+export const User = createParamDecorator(
+  (data: keyof UserEntity, ctx: ExecutionContext) => {
+    const req = ctx.switchToHttp().getRequest();
+    const { password, ...user } = req.user;
 
-  return req.user;
-});
+    return data ? user[data] : user;
+  },
+);
